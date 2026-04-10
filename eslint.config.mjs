eb51fbe85a83d5a9a00c1e99982444b18ae1ce95
All India Computer Saksharta Mission <rjits@aicsm.com>
2026-04-10 14:49:11 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,13 +6,6 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+ 
 
 export default eslintConfig;
